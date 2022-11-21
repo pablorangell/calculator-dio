f07a212d1d91d72e8ef9d95ea0aaea5ff06689ef
Pablo Rangel <prangelldestroy@gmail.com>
2022-11-21 15:38:18 -0300

    calculator

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Calculator.Models;
+
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10, 30);
+calc.Subtrair(10, 50);
+calc.Multiplicar(15, 45);
+calc.Dividir(2, 2);
+calc.Potencia(3, 3);
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
+calc.RaizQuadrada(16);
+
+
+
+
